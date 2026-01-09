cac41748caf1132fc9bfafce6ea71465ac4727e7
Jeanne Viallard <viallard.jeanne@gmail.com>
2026-01-09 15:16:04 +0100

    add profile page content

---
+++ src/components/AccountBalance/index.jsx
@@ -0,0 +1,16 @@
+export default function AccountBalance({ title, balance }) {
+  return (
+    <section className="account">
+      <div className="account-content-wrapper">
+        <h3 className="account-title">{title}</h3>
+        <p className="account-amount">
+          {balance.toLocaleString("en-US", { style: "currency", currency: "USD" })}
+        </p>
+        <p className="account-amount-description">Available Balance</p>
+      </div>
+      <div className="account-content-wrapper cta">
+        <button className="transaction-button">View transactions</button>
+      </div>
+    </section>
+  );
+}
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,3 +1,34 @@
+import { useSelector } from "react-redux";
+
+import AccountBalance from "../../components/AccountBalance";
+import accounts from "../../accounts-placeholder-data";
+
 export default function Profile() {
-  return <p>profile</p>;
+  const userFirstName = useSelector((state) => state.user.infos?.firstName);
+  const userLastName = useSelector((state) => state.user.infos?.lastName);
+
+  return (
+    <main className="main bg-dark">
+      <div className="header">
+        <h1>
+          Welcome back
+          <br />
+          {userFirstName} {userLastName}!
+        </h1>
+        <button className="edit-button">Edit Name</button>
+      </div>
+      <h2 className="sr-only">Accounts</h2>
+      <>
+        {accounts.map((account) => {
+          return (
+            <AccountBalance
+              title={account.title}
+              key={account.title}
+              balance={account.balance}
+            />
+          );
+        })}
+      </>
+    </main>
+  );
 }
