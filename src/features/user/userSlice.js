d001b5ac14df953cb5ef2e66d572a650a5342dbd
Jeanne Viallard <viallard.jeanne@gmail.com>
2026-01-09 16:48:28 +0100

    add profile infos edit

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -26,6 +26,32 @@ const fetchUser = createAsyncThunk("fetchUser", async (token, { rejectWithValue 
   }
 });
 
+const updateUser = createAsyncThunk(
+  "updateUser",
+  async ({ data, token }, { rejectWithValue }) => {
+    try {
+      const res = await fetch("http://localhost:3001/api/v1/user/profile", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+      });
+
+      const resData = await res.json();
+
+      if (!res.ok) {
+        throw new Error(resData.message);
+      }
+
+      return resData.body;
+    } catch (err) {
+      return rejectWithValue(err.message);
+    }
+  }
+);
+
 const userSlice = createSlice({
   name: "user",
   initialState,
@@ -37,10 +63,22 @@ const userSlice = createSlice({
       })
       .addCase(fetchUser.fulfilled, (state, action) => {
         state.loading = false;
-        state.error = false;
+        state.error = null;
         state.infos = action.payload;
       })
       .addCase(fetchUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(updateUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.infos = action.payload;
+      })
+      .addCase(updateUser.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
       });
@@ -48,4 +86,4 @@ const userSlice = createSlice({
 });
 
 export default userSlice.reducer;
-export { fetchUser };
+export { fetchUser, updateUser };
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,22 +1,64 @@
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { updateUser } from "../../features/user/userSlice";
 
 import AccountBalance from "../../components/AccountBalance";
 import accounts from "../../accounts-placeholder-data";
 
 export default function Profile() {
+  const dispatch = useDispatch();
+
   const userFirstName = useSelector((state) => state.user.infos?.firstName);
   const userLastName = useSelector((state) => state.user.infos?.lastName);
+  const userToken = useSelector((state) => state.auth.userToken);
 
-  return (
-    <main className="main bg-dark">
+  const [isInEditMode, setIsInEditMode] = useState(false);
+
+  let header = "";
+
+  function submitEditForm(formData) {
+    const data = {
+      firstName: formData.get("firstName"),
+      lastName: formData.get("lastName"),
+    };
+    dispatch(updateUser({ data, token: userToken }));
+    setIsInEditMode(false);
+  }
+
+  if (!isInEditMode) {
+    header = (
       <div className="header">
         <h1>
           Welcome back
           <br />
           {userFirstName} {userLastName}!
         </h1>
-        <button className="edit-button">Edit Name</button>
+        <button className="edit-button" onClick={() => setIsInEditMode(true)}>
+          Edit Name
+        </button>
       </div>
+    );
+  } else {
+    header = (
+      <div className="header">
+        <h1>Welcome back</h1>
+        <form action={submitEditForm} className="edit-infos-form">
+          <input type="text" name="firstName" required placeholder={userFirstName} />
+          <input type="text" name="lastName" required placeholder={userLastName} />
+
+          <button type="submit">Save</button>
+          <button type="button" onClick={() => setIsInEditMode(false)}>
+            Cancel
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  return (
+    <main className="main bg-dark">
+      {header}
       <h2 className="sr-only">Accounts</h2>
       <>
         {accounts.map((account) => {
